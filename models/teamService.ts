c08aa6fee555d9bcff3ef0bae4d04a31483d05c2
yuki-miyamoto1998 <63698868+yuki-miyamoto1998@users.noreply.github.com>
2023-06-11 00:34:24 +0900

    新しいメンバとセッターゲッターを追加

--- models/teamService.ts
+++ models/teamService.ts
@@ -1,7 +1,26 @@
 import { Team } from "~/models/team";
 
 export class TeamService {
+  private _createdAt: Date;
+
   constructor(private _teamList: Team[]) {
+    this._createdAt = new Date();
+  }
+
+  set teamList(value: Team[]) {
+    this._teamList = value;
+  }
+
+  set cachedAt(value: Date) {
+    this._createdAt = value;
+  }
+
+  get cachedAt(): Date {
+    return this._createdAt;
+  }
+
+  get teamList(): Team[] {
+    return this._teamList;
   }
 
   public findTeam(id: string): Team | undefined {
@@ -10,9 +29,5 @@ export class TeamService {
 
   public getTeamIdSet(): Set<string> {
     return new Set<string>(this._teamList.map(team => team.id))
-  }
-
-  get teamList(): Team[] {
-    return this._teamList;
   }
 }
